c1df066ac4d572c6b93887631ece716afd15558c
CChevalier <cchevalier70@gmail.com>
2015-07-07 12:27:10 +0200

    Start DetailActivity with Intent with EXTRA_TEXT

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.sunshine.app;
 
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -16,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -120,9 +120,18 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                // Simple Toast version
                 String forecast = mForecastAdapter.getItem(i);
+
+/*
+                // Simple Toast version
                 Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+*/
+
+                // Launch DetailActivity
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+
             }
         });
 
